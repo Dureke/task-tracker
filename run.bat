3178014cc5f70c49f7b987eb60ef233e026c673c
Dureke <dureke.sanchez2@gmail.com>
2025-11-16 08:33:57 -0600

    updated script to be modifiable at runtime

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-mvn exec:java -Dexec.mainClass="tracker.Main" -Dexec.args="update newTask"
+set COMMAND=%1
+set ARG_1=%2
+set ARG_2=%3
+
+mvn exec:java -Dexec.mainClass="tracker.Main" -Dexec.args="%COMMAND% %ARG_1% %ARG_2%"
